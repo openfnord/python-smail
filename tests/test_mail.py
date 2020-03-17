7efbe8c6962e614b2d0472f81e79d62972f42a77
Robert Habermann <mail@rhab.de>
2020-03-17 14:55:02 +0100

    fix test path

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -2,7 +2,7 @@
 import shutil
 import unittest
 import pytest
-from os import path, mkdir
+import os
 
 from tests.fixtures import get_plain_text_message
 
@@ -10,12 +10,18 @@ from tests.fixtures import get_plain_text_message
 class MailTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # Create a temporary directory
-        cls.test_dir = path.join(path.dirname(path.realpath(__file__)), "output")
-        if path.exists(cls.test_dir):
-            shutil.rmtree(cls.test_dir)
+        # use tests/output/ as target for output files
+        cls.test_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output")
+        if not os.path.exists(cls.test_dir):
+            os.mkdir(cls.test_dir)
         else:
-            mkdir(cls.test_dir)
+            # directory exists - remove all files in it
+            for file_object in os.listdir(cls.test_dir):
+                file_object_path = os.path.join(cls.test_dir, file_object)
+                if os.path.isfile(file_object_path) or os.path.islink(file_object_path):
+                    os.unlink(file_object_path)
+                else:
+                    shutil.rmtree(file_object_path)
 
     # def tearDown(self):
     #     # Remove the directory after the test
@@ -27,29 +33,25 @@ class MailTest(unittest.TestCase):
     def plain_text_message(cls):
         cls.plain_text_message = get_plain_text_message()
 
-    def test_something(self):
-        # Create a file path
-        file_path = path.join(self.test_dir, 'test1.txt')
-
-        # Create a file in the temporary directory
-        with open(file_path, 'w') as f:
-            # Write something to it
-            f.write('The owls are not what they seem1')
-
-        # Reopen the file and check if what we read back is the same
-        with open(file_path) as f:
-            self.assertEqual(f.read(), 'The owls are not what they seem1')
+    # def test_something(self):
+    #     # Create a file path
+    #     file_path = os.path.join(self.test_dir, 'test1.txt')
+    #
+    #     # Create a file in the temporary directory
+    #     with open(file_path, 'w') as f:
+    #         # Write something to it
+    #         f.write('The owls are not what they seem1')
+    #
+    #     # Reopen the file and check if what we read back is the same
+    #     with open(file_path) as f:
+    #         self.assertEqual(f.read(), 'The owls are not what they seem1')
 
     # def test_dir_path(self):
     #     # Create a file path
     #     self.assertEqual(self.test_dir, "foobar")
 
     def test_plain_text_message(self):
-        file_path = path.join(self.test_dir, 'plain_text_message.eml')
-
-        print("---")
-        print("Test DIR: {}".format(self.test_dir))
-        print("---")
+        file_path = os.path.join(self.test_dir, 'plain_text_message.eml')
 
         # Create a file in the temporary directory
         with open(file_path, 'wb') as f:
