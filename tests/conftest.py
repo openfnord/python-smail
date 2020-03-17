c4b6113dbf73c4124c87b7c922562b2c4864d96b
Robert Habermann <mail@rhab.de>
2020-03-17 14:00:13 +0100

    pep8 fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,4 @@
 # _*_ coding: utf-8 _*_
 import os
-from email.mime.text import MIMEText
-from email.utils import formatdate
-
-import pytest
 
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'testdata')
