94c6163f806c59b964218b0050a4f7f97c108645
Robert Habermann <mail@rhab.de>
2020-03-17 14:09:47 +0100

    find path

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -36,21 +36,12 @@ class MailTest(unittest.TestCase):
         with open(file_path) as f:
             self.assertEqual(f.read(), 'The owls are not what they seem1')
 
-    def test_something2(self):
-        # Create a file path
-        file_path = path.join(self.test_dir, 'test2.txt')
-
-        # Create a file in the temporary directory
-        with open(file_path, 'w') as f:
-            # Write something to it
-            f.write('The owls are not what they seem2')
-
-        # Reopen the file and check if what we read back is the same
-        with open(file_path) as f:
-            self.assertEqual(f.read(), 'The owls are not what they seem2')
-
     def test_plain_text_message(self):
         file_path = path.join(self.test_dir, 'plain_text_message.eml')
+
+        print("---")
+        print("Test DIR: {}".format(self.test_dir))
+        print("---")
 
         # Create a file in the temporary directory
         with open(file_path, 'wb') as f:
