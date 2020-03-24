38c3af800d1ca879088380dbea4a698ee08b2b0e
Robert Habermann <mail@rhab.de>
2020-03-24 12:27:54 +0100

    remove unittest

--- tests/test_cert.py
+++ tests/test_cert.py
@@ -1,51 +1,94 @@
 # _*_ coding: utf-8 _*_
 import os
-import unittest
 
-from .conftest import FIXTURE_DIR
-
-from smail.cert import Certificate, key_and_certs_from_pkcs12
-# from cryptography.hazmat.backends.openssl.rsa import _RSAPrivateKey
+from asn1crypto import x509, pem
 from cryptography.hazmat.backends.openssl.rsa import RSAPrivateKeyWithSerialization
 
+from smail.cert import Certificate, key_and_certs_from_pkcs12
+from .conftest import FIXTURE_DIR
 
-class CertTest(unittest.TestCase):
 
-    def setUp(self):
-        self.openssl_binary = os.environ.get("OPENSSL_BINARY", None)
-        if not self.openssl_binary:
-            self.openssl_binary = "openssl"
+class TestCert:
+
+    # @classmethod
+    # def setup_class(cls):
+    #     """ setup any state specific to the execution of the given class (which
+    #     usually contains tests).
+    #     """
+    #
+    #     cls.openssl_binary = os.environ.get("OPENSSL_BINARY", None)
+    #     if not cls.openssl_binary:
+    #         cls.openssl_binary = "openssl"
+    #
+    # @classmethod
+    # def teardown_class(cls):
+    #     """ teardown any state that was previously setup with a call to
+    #     setup_class.
+    #     """
+    #     pass
 
     def test_cert_from_pem_file(self):
         cert = Certificate.from_pem_file(os.path.join(FIXTURE_DIR, 'CarlRSASelf.pem'))
-        self.assertIsInstance(cert, Certificate)
-        self.assertTrue(cert.self_signed)
+        assert isinstance(cert, Certificate)
+        assert cert.self_signed
 
     def test_cert_from_pem(self):
         with open(os.path.join(FIXTURE_DIR, 'CarlRSASelf.pem'), 'rb') as f:
             cert = Certificate.from_pem(f.read())
-            self.assertIsInstance(cert, Certificate)
+            assert isinstance(cert, Certificate)
 
-    def test_cert_from_pem_str_raises_exception(self):
-        with open(os.path.join(FIXTURE_DIR, 'CarlRSASelf.pem')) as f:
-            with self.assertRaises(TypeError):
-                Certificate.from_pem(f.read())
+    # def test_cert_from_pem_str_raises_exception(self):
+    #     with open(os.path.join(FIXTURE_DIR, 'CarlRSASelf.pem')) as f:
+    #         with self.assertRaises(TypeError):
+    #             Certificate.from_pem(f.read())
 
     def test_cert_from_der_file(self):
         cert = Certificate.from_der_file(os.path.join(FIXTURE_DIR, 'google_cert.der'))
-        self.assertIsInstance(cert, Certificate)
+        assert isinstance(cert, Certificate)
 
     def test_cert_from_der(self):
         with open(os.path.join(FIXTURE_DIR, 'google_cert.der'), 'rb') as f:
             cert = Certificate.from_der(f.read())
-            self.assertIsInstance(cert, Certificate)
+            assert isinstance(cert, Certificate)
 
     def test_key_and_certs_from_pkcs12(self):
         with open(os.path.join(FIXTURE_DIR, 'BobRSASignByCarl_password.p12'), 'rb') as f:
             key, cert, certs = key_and_certs_from_pkcs12(f.read(),
                                                          password="password")
 
-        self.assertIsInstance(key, RSAPrivateKeyWithSerialization)
-        self.assertIsInstance(cert, Certificate)
-        self.assertEqual(len(certs), 1)
-        self.assertIsInstance(certs[0], Certificate)
+        assert isinstance(key, RSAPrivateKeyWithSerialization)
+        assert isinstance(cert, Certificate)
+        assert len(certs) == 1
+        assert isinstance(certs[0], Certificate)
+
+    def test_recipient_info(self):
+        with open(os.path.join(FIXTURE_DIR, 'AliceRSASignByCarl.pem'), 'rb') as cert_signer_file:
+            der_bytes = cert_signer_file.read()
+
+            if pem.detect(der_bytes):
+                type_name, headers, der_bytes = pem.unarmor(der_bytes)
+
+            asn1crypto_cert = x509.Certificate.load(der_bytes)
+
+        with open(os.path.join(FIXTURE_DIR, 'AliceRSASignByCarl.pem'), 'rb') as cert_file:
+            bytes_cert = cert_file.read()
+
+        smail_cert = Certificate.from_pem_file(os.path.join(FIXTURE_DIR, 'AliceRSASignByCarl.pem'))
+
+        assert isinstance(bytes_cert, bytes)
+        assert isinstance(asn1crypto_cert, x509.Certificate)
+        assert isinstance(smail_cert, Certificate)
+        assert smail_cert.self_signed == asn1crypto_cert.self_issued
+
+        # session_key = os.urandom(16)
+        # cipher = get_cipher("aes256_cbc")
+        # # encrypted_key = cipher.encrypt(session_key)
+        #
+        # ri1 = load_recipient_info(asn1crypto_cert, session_key)
+        # assert isinstance(ri1, RecipientInfo)
+        #
+        # ri2 = iterate_recipient_infos(bytes_cert, session_key)
+        # for ri2_item in ri2:
+        #     assert isinstance(ri2_item, RecipientInfo)
+        #
+        #     assert ri1.parse() == ri2_item.parse()
