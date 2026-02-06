5e41d5d90e80b479f6e079ed296d6909ee7350e9
openfnord <luja@openhardware.de>
2026-02-06 17:01:18 +0100

    improved random numbers by using oscrypto util.rand_bytes

--- smail/ciphers.py
+++ smail/ciphers.py
@@ -5,6 +5,7 @@ from abc import ABCMeta
 from abc import abstractmethod
 
 from oscrypto import symmetric
+from oscrypto import util
 
 
 class EncryptionCipher:
@@ -30,8 +31,15 @@ class AesCbc(EncryptionCipher):
     def __init__(self, algorithm, key_size):
         self.algorithm = algorithm
         self.key_size = key_size
-        self._session_key = os.urandom(self.key_size)
-        self._iv = os.urandom(16)  # fixed size of 16 bytes (block size) for initialization vector
+
+        #improve random numbers 
+        #self._session_key = os.urandom(self.key_size)
+        #self._iv = os.urandom(16)  # fixed size of 16 bytes (block size) for initialization vector
+
+        #https://github.com/wbond/oscrypto/blob/master/docs/util.md
+        self._session_key = util.rand_bytes(self.key_size)
+        self._iv = util.rand_bytes(16)  # fixed size of 16 bytes (block size) for initialization vector
+
 
     def encrypt(self, data):
         _, ciphertext = symmetric.aes_cbc_pkcs7_encrypt(self._session_key, data.encode("utf-8"), self._iv)
@@ -61,8 +69,14 @@ class TripleDes(EncryptionCipher):
     def __init__(self, algorithm, key_size):
         self.algorithm = algorithm
         self.key_size = key_size
-        self._session_key = os.urandom(self.key_size)
-        self._iv = os.urandom(8)  # fixed size of 8 bytes for initialization vector
+        
+        #improve random numbers 
+        #self._session_key = os.urandom(self.key_size)
+        #self._iv = os.urandom(8)  # fixed size of 8 bytes for initialization vector
+
+        #https://github.com/wbond/oscrypto/blob/master/docs/util.md
+        self._session_key = util.rand_bytes(self.key_size)
+        self._iv = util.rand_bytes(8)  # fixed size of 8 bytes for initialization vector
 
     def encrypt(self, data):
         _, ciphertext = symmetric.tripledes_cbc_pkcs5_encrypt(self._session_key, data.encode("utf-8"), self._iv)
